f2b0b95ed175285d3bcc94ddf1f0faba997b8b37
HUONGNV-VANTIX\NGUYENVANHUONG <v.huongnv9@vantixinc.com>
2020-12-20 16:50:29 +0700

    wifi connect to AP

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ESP32_GATEWAY
 Gateway BaseOn ESP32
+
+How to connection
+Connection : https://www.instructables.com/ST7920-128X64-LCD-Display-to-ESP32/
+LCD sch : https://reprap.org/mediawiki/images/5/51/RRD_FULL_GRAPHIC_SMART_CONTROLER_SCHEMATIC.pdf
+Wifi check: https://savjee.be/2020/02/esp32-keep-wifi-alive-with-freertos-task/
+RTOS doc: https://laubugs.wordpress.com/2019/06/09/su-dung-freertos-choi-luon-phan-1/
--- main/main.ino
+++ main/main.ino
@@ -6,16 +6,27 @@
 
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include "WiFi.h"
+
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 2
 #endif
+#define LCD_CLK 18 //GPIO 18
+#define LCD_DATA 21 //GPIO21
+#define LCD_CS 05 //GPIO05
+
+#define WIFI_NETWORK "TuyenHaT1"
+#define WIFI_PASSWORD "12345654"
+#define WIFI_CHECKTIME 10000
+#define WIFI_TIMEOUT_MS 20000
+#define WIFI_RECOVER_TIME_MS 30000
 
 // define task
-void TaskWifi( void *pvParameters );
+void TaskWiFi( void *pvParameters );
 void TaskDisplay( void *pvParameters );
 void TaskStatusLed( void *pvParameters);
 // setup lcd and lcd function
-U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 18 /* A4 */ , /* data=*/ 21 /* A2 */, /* CS=*/ 05 /* A3 */, /* reset=*/ U8X8_PIN_NONE);
+U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ LCD_CLK /* A4 */ , /* data=*/ LCD_DATA /* A2 */, /* CS=*/ LCD_CS /* A3 */, /* reset=*/ U8X8_PIN_NONE);
 void u8g2_prepare(void) {
   u8g2.setFont(u8g2_font_6x10_tf);
   u8g2.setFontRefHeightExtendedText();
@@ -34,9 +45,15 @@ void u8g2_ascii_1() {
     }
   }
 }
+// End lcd fucntion
+// WiFi function 
+
+// End WiFi function
+// Begin lcd screen
 void mainScreen(){
   u8g2.drawStr( 0, 0, "test");
   }
+// End lcd screen
 // Status led 
 void statusLed(int ton,int toff){
   digitalWrite(LED_BUILTIN, HIGH);   
@@ -50,15 +67,15 @@ void setup() {
   Serial.begin(115200);
   u8g2.begin();
 
-  xTaskCreatePinnedToCore(TaskWifi, "TaskWifi", 1024, NULL, 2, NULL, ARDUINO_RUNNING_CORE);
-  xTaskCreatePinnedToCore(TaskDisplay, "TaskDisplay", 1024, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
+  xTaskCreatePinnedToCore(TaskWiFi, "TaskWiFi", 3072, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
+  xTaskCreatePinnedToCore(TaskDisplay, "TaskDisplay", 1024, NULL, 2, NULL, ARDUINO_RUNNING_CORE);
   xTaskCreatePinnedToCore(TaskStatusLed, "TaskStatusLed", 512, NULL, 3, NULL, ARDUINO_RUNNING_CORE);
   
 }
 // loop loop 
 void loop()
 {
-  // Do not work
+  // Do not place code here
 }
 /*---------------------- Tasks ---------------------*/
 void TaskStatusLed(void *pvParameters){
@@ -70,11 +87,34 @@ void TaskStatusLed(void *pvParameters){
       vTaskDelay(1);
       }
   }
-void TaskWifi(void *pvParameters){
+void TaskWiFi(void *pvParameters){
     (void) pvParameters;
     for (;;) {
-      vTaskDelay(1);
-      }
+        if (WiFi.status() == WL_CONNECTED){
+             vTaskDelay(WIFI_CHECKTIME);
+             Serial.print("Still connect to ");
+             Serial.println(WIFI_NETWORK);
+             continue;
+          }
+        Serial.print("Try to connect: ");
+        Serial.println(WIFI_NETWORK);
+        WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+        unsigned long startAttemptTime = millis();
+        while (WiFi.status() != WL_CONNECTED && (millis() - startAttemptTime) < WIFI_TIMEOUT_MS) {
+            vTaskDelay(1000);
+            Serial.println("Disconnected from wifi AP");
+          }
+        if (WiFi.status() != WL_CONNECTED){
+             vTaskDelay(WIFI_RECOVER_TIME_MS);
+             Serial.println("Check again network ");
+             Serial.println(WIFI_NETWORK);
+             continue;
+          }
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+        vTaskDelay(1);
+        }
   }
 void TaskDisplay(void *pvParameters){ // Run Diskplay Task
     (void) pvParameters;
@@ -83,10 +123,10 @@ void TaskDisplay(void *pvParameters){ // Run Diskplay Task
       
       u8g2.clearBuffer();
       u8g2_prepare();
-      u8g2_ascii_1();
+      //u8g2_ascii_1();
       mainScreen();
       u8g2.sendBuffer();
-      vTaskDelay(1000);
+      vTaskDelay(20);
       }
     
   }
