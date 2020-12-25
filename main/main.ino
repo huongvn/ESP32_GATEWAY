8b1276e6fbcca9f8a9f1f72b7b2d7c67a2947a5f
huongvn <huongvn.tdh@gmail.com>
2020-12-25 22:37:07 +0700

    combine lcd,wifi,led

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,18 @@
 
-#define ARDUINO_RUNNING_CORE 1
+#define ARDUINO_ONE_CORE 1
 #define ARDUINO_ZERO_CORE 0
 
 #include <Arduino.h>
-#include <U8g2lib.h>
 #include "WiFi.h"
+#include "Libraries//ST7290.h"
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 2
 #endif
-#define LCD_CLK 18 //GPIO 18
-#define LCD_DATA 21 //GPIO21
-#define LCD_CS 05 //GPIO05
 
-#define WIFI_NETWORK "TuyenHaT1"
-#define WIFI_PASSWORD "12345654"
+
+#define WIFI_NETWORK "_"
+#define WIFI_PASSWORD "_"
 #define WIFI_CHECKTIME 10000
 #define WIFI_TIMEOUT_MS 20000
 #define WIFI_RECOVER_TIME_MS 30000
@@ -24,33 +22,12 @@ void TaskWiFi( void *pvParameters );
 void TaskDisplay( void *pvParameters );
 void TaskStatusLed( void *pvParameters);
 // setup lcd and lcd function
-U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ LCD_CLK /* A4 */ , /* data=*/ LCD_DATA /* A2 */, /* CS=*/ LCD_CS /* A3 */, /* reset=*/ U8X8_PIN_NONE);
-void u8g2_prepare(void) {
-  u8g2.setFont(u8g2_font_6x10_tf);
-  u8g2.setFontRefHeightExtendedText();
-  u8g2.setDrawColor(1);
-  u8g2.setFontPosTop();
-  u8g2.setFontDirection(0);
-}
-void u8g2_ascii_1() {
-  char s[2] = " ";
-  uint8_t x, y;
-  u8g2.drawStr( 0, 0, "ASCII page 1");
-  for( y = 0; y < 6; y++ ) {
-    for( x = 0; x < 16; x++ ) {
-      s[0] = y*16 + x + 32;
-      u8g2.drawStr(x*7, y*10+10, s);
-    }
-  }
-}
 // End lcd fucntion
 // WiFi function 
 
 // End WiFi function
 // Begin lcd screen
-void mainScreen(){
-  u8g2.drawStr( 0, 0, "test");
-  }
+
 void WifiScreen(){
   
   }
@@ -67,11 +44,10 @@ void statusLed(int ton,int toff){
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
-  u8g2.begin();
-  u8g2_prepare();
-  xTaskCreatePinnedToCore(TaskWiFi, "TaskWiFi", 4096, NULL, 1, NULL, ARDUINO_ZERO_CORE);
-  xTaskCreatePinnedToCore(TaskDisplay, "TaskDisplay", 4096, NULL, 2, NULL, ARDUINO_RUNNING_CORE);
-  xTaskCreatePinnedToCore(TaskStatusLed, "TaskStatusLed", 1024, NULL, 3, NULL, ARDUINO_RUNNING_CORE);
+  ST7290setup();
+  xTaskCreatePinnedToCore(TaskWiFi, "TaskWiFi", 4096, NULL, 1, NULL, ARDUINO_ONE_CORE);
+  xTaskCreatePinnedToCore(TaskDisplay, "TaskDisplay", 4096, NULL, 2, NULL, ARDUINO_ONE_CORE);
+  xTaskCreatePinnedToCore(TaskStatusLed, "TaskStatusLed", 1024, NULL, 3, NULL, ARDUINO_ONE_CORE);
   Serial.println("Setup done");
 }
 // loop loop 
@@ -94,7 +70,7 @@ void TaskWiFi(void *pvParameters){
     (void) pvParameters;
     for (;;) {
       int n = WiFi.scanNetworks();
-      Serial.println("scan done");
+      Serial.println("DEBUG: scan done");
       if (n == 0) {
         Serial.println("no networks found");
       } else {
@@ -119,10 +95,7 @@ void TaskWiFi(void *pvParameters){
 void TaskDisplay(void *pvParameters){ // Run Diskplay Task
     (void) pvParameters;
     for(;;) {
-      char *string_list = "aab\n";
-      u8g2.userInterfaceSelectionList("NETWORK FOUND", 1, string_list);
+      test16x16();
       vTaskDelay(20);
-      Serial.println("DEBUG: EXIT TASK DISPLAY");
-      vTaskSuspend( NULL );
       }
   }
---
+++ test/SPI_Multiple_Buses/SPI_Multiple_Buses.ino
@@ -0,0 +1,146 @@
+
+
+/* The ESP32 has four SPi buses, however as of right now only two of
+ * them are available to use, HSPI and VSPI. Simply using the SPI API 
+ * as illustrated in Arduino examples will use VSPI, leaving HSPI unused.
+ * 
+ * However if we simply intialise two instance of the SPI class for both
+ * of these buses both can be used. However when just using these the Arduino
+ * way only will actually be outputting at a time.
+ * 
+ * Logic analyser capture is in the same folder as this example as
+ * "multiple_bus_output.png"
+ * 
+ * created 30/04/2018 by Alistair Symonds
+ */
+#include <Arduino.h>
+#include <SPI.h>
+
+static const int spiClk = 100000; // 0.2 MHz
+
+#define LCD_CS          5
+#define LCD_CLS         0x01
+#define LCD_HOME        0x02
+#define LCD_ADDRINC     0x06
+#define LCD_DISPLAYON   0x0C
+#define LCD_CURSORON    0x0E
+#define LCD_CURSORBLINK 0x0F
+#define LCD_BASIC       0x30
+#define LCD_EXTEND      0x34
+#define LCD_LINE0       0x80
+#define LCD_LINE1       0x90
+#define LCD_LINE2       0x88
+#define LCD_LINE3       0x98
+
+//uninitalised pointers to SPI objects
+SPIClass * vspi = NULL;
+void sendCmd(byte b) 
+{
+  vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
+  digitalWrite(LCD_CS, HIGH);
+  vspi->transfer(0xF8);
+  vspi->transfer(b & 0xF0);
+  vspi->transfer(b << 4);
+  digitalWrite(LCD_CS, LOW);
+  vspi->endTransaction();
+}
+void sendData(byte b) 
+{
+  vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
+  digitalWrite(LCD_CS, HIGH);
+  vspi->transfer(0xFA);
+  vspi->transfer(b & 0xF0);
+  vspi->transfer(b << 4);
+  digitalWrite(LCD_CS, LOW);
+  vspi->endTransaction();
+}
+void LCD_init() 
+{
+  //digitalWrite(LCD_RST, LOW);
+  //delay(100);
+  //digitalWrite(LCD_RST, HIGH);
+
+  sendCmd(LCD_BASIC);
+  sendCmd(LCD_BASIC);
+  sendCmd(LCD_CLS); delay(2);
+  sendCmd(LCD_ADDRINC);
+  sendCmd(LCD_DISPLAYON);
+}
+void vspiCommand() {
+  byte data = 0b01010101; // junk data to illustrate usage
+
+  //use it as you would the regular arduino SPI API
+  vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
+  digitalWrite(5, LOW); //pull SS slow to prep other end for transfer
+  vspi->transfer(data);  
+  digitalWrite(5, HIGH); //pull ss high to signify end of data transfer
+  vspi->endTransaction();
+}
+
+void setup() {
+  //initialise two instances of the SPIClass attached to VSPI and HSPI respectively
+  vspi = new SPIClass(VSPI);
+
+  //clock miso mosi ss
+  //initialise vspi with default pins
+  //SCLK = 18, MISO = 19, MOSI = 23, SS = 5
+  vspi->begin();
+  pinMode(LCD_CS, OUTPUT); //VSPI SS
+  digitalWrite(LCD_CS, LOW);
+  LCD_init();
+}
+char buf[20];
+
+void test8x16()
+{
+  int del = 2000;
+  sendCmd(LCD_BASIC);
+  unsigned int i,j,c=0;
+  for(i=0;i<0x80;i++) {
+    c++;
+    if(c==16) sendCmd(LCD_LINE2);
+    else if(c==32) sendCmd(LCD_LINE3);
+    else if(c>=48 || i==0) {
+      if(c>=48) delay(del);
+      snprintf(buf,20,"%02X..%02X",i,i+47);
+      sendCmd(LCD_CLS); delay(2);
+      for(j=0;j<strlen(buf);j++) sendData(buf[j]);
+      c=0;
+      sendCmd(LCD_LINE1);
+    }
+    sendData(i==0?' ':i);
+  }
+  delay(del);
+}
+void test16x16()
+{
+  int del = 300;
+  sendCmd(LCD_BASIC);
+  unsigned int i,j,c=0;
+  int st=0xa140;
+  for(i=st;i<0xffff;i++) {
+    c++;
+    if(c==8) sendCmd(LCD_LINE2);
+    else if(c==16) sendCmd(LCD_LINE3);
+    else if(c>=24 || i==st) {
+      if(c>=24) delay(i<0xac00?800:del);
+      snprintf(buf,20,"%04X..%04X",i,i+23);
+      sendCmd(LCD_CLS); delay(2);
+      for(j=0;j<strlen(buf);j++) sendData(buf[j]);
+      c=0;
+      sendCmd(LCD_LINE1);
+    }
+    sendData(i>>8);
+    sendData(i&0xff);
+  }
+  delay(del);
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  //use the SPI buses
+  test8x16();
+  test16x16();
+  delay(100);
+}
+
